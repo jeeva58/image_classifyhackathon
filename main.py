ebb4fb0cfc226c5d9255a3b2ce4ed40e731ff315
Jeeva Jaleesh <85050350+jeeva58@users.noreply.github.com>
2021-10-24 07:20:31 +0530

    Update main.py

--- main.py
+++ main.py
@@ -2,5 +2,5 @@ from simple_image_download import simple_image_download as sim
 
 response = sim.simple_image_download
 
-response().download('bad oranges', 10)
-response().download('good oranges', 10)
+response().download('bad oranges', 40)
+response().download('good oranges', 40)
